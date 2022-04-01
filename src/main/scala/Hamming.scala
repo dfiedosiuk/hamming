b02726999a563539e0260f8599b70c2c2fb5dbb9
Daniel <d.fiedosiuk@adform.com>
2022-04-01 12:17:43 +0200

    Created Hamming.calculate(). All test passed

--- src/main/scala/Hamming.scala
+++ src/main/scala/Hamming.scala
@@ -1,4 +1,16 @@
-object Hamming extends App{
+object Hamming extends App {
 
-  def calculate(origin: String, replica: String) = 7
+  def calculate(origin: String, replica: String): Option[Int] =
+    if (origin.length == replica.length) {
+    val differences = origin.zip(replica).foldLeft("") { (result, n) =>
+      if (n._1 == n._2) result
+      else result + n._1
+    }
+      println(s"Hamming distance is ${differences.length}")
+    Some(differences.length)
+  } else {
+      println("Invalid input! ")
+      None
+    }
 }
+
--- src/test/scala/HammingSpec.scala
+++ src/test/scala/HammingSpec.scala
@@ -9,7 +9,9 @@ class HammingSpec extends AnyFlatSpec with should.Matchers {
   behavior of "A Hamming.calculate() "
 
   it should "return the Hamming Distance between two DNA strands " in {
-    calculate("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT") shouldBe 7
+    calculate("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT") shouldBe Some(7)
+    calculate("GAGCCTACTAACGGGCT", "CATCGTAATGACGGCCT") shouldBe Some(6)
+    calculate("GAGCCTACTAACGGGATA", "CATCGTAATGACGGCCT") shouldBe None
   }
 
 }
