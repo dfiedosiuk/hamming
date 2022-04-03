cd952aa8316324fcccef191020b2d17d165516d4
Daniel <d.fiedosiuk@adform.com>
2022-04-03 20:12:41 +0200

    More tests added. DNA letters requirement added.

--- src/main/scala/Hamming.scala
+++ src/main/scala/Hamming.scala
@@ -1,16 +1,19 @@
 object Hamming extends App {
 
-  def calculate(origin: String, replica: String): Option[Int] =
-    if (origin.length == replica.length) {
-    val differences = origin.zip(replica).foldLeft("") { (result, n) =>
-      if (n._1 == n._2) result
-      else result + n._1
-    }
+  def calculate(origin: String, replica: String): Option[Int] = {
+    val dnaLetters = List('C','A','G','T')
+    if (origin.length == replica.length && (origin++replica).forall(dnaLetters.contains)) {
+      val differences = origin.zip(replica).foldLeft("") { (result, n) =>
+        if (n._1 == n._2) result
+        else result + n._1
+      }
       println(s"Hamming distance is ${differences.length}")
-    Some(differences.length)
-  } else {
+      Some(differences.length)
+    } else {
       println("Invalid input! ")
       None
     }
+  }
+
 }
 
--- src/test/scala/HammingSpec.scala
+++ src/test/scala/HammingSpec.scala
@@ -11,7 +11,13 @@ class HammingSpec extends AnyFlatSpec with should.Matchers {
   it should "return the Hamming Distance between two DNA strands " in {
     calculate("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT") shouldBe Some(7)
     calculate("GAGCCTACTAACGGGCT", "CATCGTAATGACGGCCT") shouldBe Some(6)
+    calculate("AAAA", "AAAA") shouldBe Some(0)
+  }
+
+  it should "return \"Invalid input!\" and return None " in {
     calculate("GAGCCTACTAACGGGATA", "CATCGTAATGACGGCCT") shouldBe None
+    calculate("AAA", "AA") shouldBe None
+    calculate("abc", "abb") shouldBe None
   }
 
 }
